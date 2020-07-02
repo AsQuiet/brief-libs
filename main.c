8ee0e9efb8df9c27ce1e4c7da9398a779f74279b
AsQuiet <65712624+AsQuiet@users.noreply.github.com>
2020-07-02 16:58:42 +0200

    uhm

--- main.c
+++ main.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "briefc/bio.h"
-
-int str_len(char *line) {
-    int length = 0;
-
-    while (*(line)) {
-        length = length + 1;
-        (line++);
-    }
-
-    return length;
-}
 
 int main() {
 
-    int len = str_len("quinten");
-    printf("%d", len);
+    printf("LOLOLOLOLOLOL\n");
 
-    // int amt = bio_get_file_lines("d.txt");
-    // char **lines = bio_alloc_lines_of_file("d.txt", amt,20);
-    // bio_print_lines_of_file(lines, amt);
+    char input[50];
+    fgets(input, 50, stdin);
+
+    printf("\nlol is %s", input);
 
     return 0;
 }
+
